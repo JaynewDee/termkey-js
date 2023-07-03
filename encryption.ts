484f05db227ad094e81c6921d72212e682a4bff8
NuminousBlue <jdiehl2236@gmail.com>
2023-07-03 00:38:56 -0500

    aes-gcm was source of decryption bug strangeness, despite matching values produced | switched to cbc and seems remedied | added tyescript support, ts-node, node types

--- encryption.ts
+++ encryption.ts
@@ -1,21 +1,22 @@
-const {
+import {
   randomBytes,
   createCipheriv,
   createDecipheriv,
   pbkdf2Sync
-} = require('crypto')
-
+} from 'crypto'
 // Define encryption algorithm to use
-const ALGO = 'aes-256-gcm'
+const ALGO = 'aes-256-cbc'
 // AES256-GCM
 // Advanced Encryption Standard in Galois Counter Mode
-const iv = Buffer.from(randomBytes(16))
 
-// The use of an IV prevents the repetition of a sequence of text in data encryption. Specifically, during encryption, an IV prevents a sequence of plaintext that's identical to a previous plaintext sequence from producing the same ciphertext.
+// Initialization Vector
+// The use of an IV prevents the repetition of a sequence of text in data encryption.
+// Specifically, during encryption, an IV prevents a sequence of plaintext that's identical to
+// a previous plaintext sequence from producing the same ciphertext.
 
 // Key is securely generated using pbkdf2, a key derivation function with a sliding computational cost,
 // used to reduce vulnerability to brute-force attacks
-function keygen () {
+function keygen() {
   // "Password" entropy determines cryptographic operations applied to key
   const entropy = Buffer.from(randomBytes(16))
   // Should be random and at least 16 bytes long
@@ -26,17 +27,20 @@ function keygen () {
 
 //
 
-function encrypt (plainText, key) {
+function encrypt(plainText: string, key: Buffer, iv: Buffer): string {
+  // Create, update, and finalize are separate operations to allow
+  // for streaming, a welcome feature when encrypting/decrypting large data sets
   const cipher = createCipheriv(ALGO, key, iv)
+  // update takes data/plainText, inputEncoding, outputEncoding
   return cipher.update(plainText, 'utf8', 'hex') + cipher.final('hex')
 }
 
-function decrypt (encryptedText, key, iv) {
+function decrypt(encryptedText: string, key: Buffer, iv: Buffer): string {
   const decipher = createDecipheriv(ALGO, key, iv)
   return decipher.update(encryptedText, 'hex', 'utf8') + decipher.final('utf8')
 }
 
-module.exports = {
+export {
   keygen,
   encrypt,
   decrypt
---
+++ io.ts
@@ -0,0 +1,83 @@
+import { writeFile, readFile } from 'fs/promises';
+import { extname } from 'path';
+const { exit } = process
+
+const VALID_FILE_TYPES = ['.txt', '.bin']
+
+function isValidFileName(shouldKeygen: boolean, filename: string) {
+  if (shouldKeygen) return true
+
+  const extension = extname(filename)
+  return VALID_FILE_TYPES.includes(extension)
+}
+
+function FileHandler() {
+  return {
+    readText: async (filename: string) => await readFile(filename),
+    readCipherText: async (filename: string) => {
+      const cipherAndIv = await readFile(filename)
+
+      const [text, iv] = cipherAndIv.toString('utf8').split(',')
+      return [text, iv]
+    },
+    writeKey: async (key: Buffer) => await writeFile('cipher_key.bin', key),
+    writePlainText: async (filename: string, text: string) => await writeFile(filename, text),
+    writeCipherText: async (filename: string, cipher: string, iv: Buffer) => {
+      const writeable = cipher + ',' + iv.toString('hex')
+
+      await writeFile(filename, writeable)
+
+      console.log(`Cipher written to ${filename}`)
+    },
+  }
+}
+
+/*
+  Arguments Format: 
+  node main.js <operation> <path_to_txt_file>>
+*/
+
+function processArgs(): [string, string] {
+  const args = process.argv
+
+  if (args.length < 3) {
+    console.error('No argument passed!')
+    exit(1)
+  }
+
+  let op = args[2]
+
+  const shouldEncrypt = op === 'e' || op === 'encrypt'
+  const shouldDecrypt = op === 'd' || op === 'decrypt'
+  const shouldKeygen = op === 'gen'
+
+  const invalidOp = !shouldEncrypt && !shouldDecrypt && !shouldKeygen
+
+  if (invalidOp) {
+    console.error(`Invalid Operation ::: ${op}`)
+    exit(1)
+  }
+
+  const target = args[3]
+
+  if (!isValidFileName(shouldKeygen, target)) {
+    console.error(`No support for filetype ${target}`)
+    exit(1)
+  }
+
+  if (shouldEncrypt) {
+    op = 'encrypt'
+  }
+
+  if (shouldDecrypt) {
+    op = 'decrypt'
+  }
+
+  if (shouldKeygen) {
+    op = 'keygen'
+  }
+
+  return [op, target]
+}
+
+export { processArgs, FileHandler }
